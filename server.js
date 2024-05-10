3d432301ac86d7464aa66c8f3dc92afd623d8b19
Shubham Patil <shubhampatil871@gmail.com>
2024-05-10 13:44:36 +0530

    add root and about route

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
-
-
 app.get("/", (req, res) => {
+  console.log(req.url);
   res.send("Hello from express!");
+});
+app.get("/about", (req, res) => {
+  res.send("This is about page");
 });
 
 app.listen(PORT, () => {
