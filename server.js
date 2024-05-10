121265278c940ab727566168dc97d7f96299cd13
Shubham Patil <shubhampatil871@gmail.com>
2024-05-10 17:57:09 +0530

    add post route

--- server.js
+++ server.js
@@ -4,14 +4,35 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Route handler for the root URL ("/")
-app.get("/", (req, res) => {
-  res.send("Home page ");
-});
+let posts = [
+  { id: 1, title: "post one" },
+  { id: 2, title: "post two" },
+  { id: 3, title: "post three" },
+];
 
 // Route handler for the "/about" URL
-app.get("/about", (req, res) => {
-  res.send("This is a about page");
+app.get("/", (req, res) => {
+  res.json({ message: "This is a home page" });
+});
+
+// Route handler for the root URL ("/")
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.get("/api/posts/:id", (req, res) => {
+  try {
+    const postId = parseInt(req.params.id);
+    const singlePost = posts.find((post) => post.id == postId);
+
+    if (!singlePost) {
+      return res.status(404).json({ message: "post not found" });
+    }
+
+    res.json(singlePost);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 app.listen(PORT, () => {
