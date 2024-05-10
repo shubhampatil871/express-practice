51bc15f8bbeecd8e7204a425cb0bd8f3d81cf138
Shubham Patil <shubhampatil871@gmail.com>
2024-05-10 14:20:10 +0530

    server html files

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Route handler for the root URL ("/")
 app.get("/", (req, res) => {
-  console.log(req.url);
-  res.send("Hello from express!");
+  res.send("Home page ");
 });
+
+// Route handler for the "/about" URL
 app.get("/about", (req, res) => {
-  res.send("This is about page");
+  res.send("This is a about page");
 });
 
 app.listen(PORT, () => {
-  console.log("server running on port", PORT);
+  console.log("Server running on port", PORT);
 });
